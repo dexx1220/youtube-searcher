980a15b7e107a6c36263a04132c35dca0a87d622
dexx1220 <dexx1220@gmail.com>
2019-08-18 13:03:33 -0400

    refactoring with Promise.all

--- index.js
+++ index.js
@@ -1,70 +1,68 @@
 const moment = require('moment')
-const videoList = require('./config/videoList')
 const youtube = require('./lib/youtube')
 const store = require('./lib/store')
+var videoList = require('./config/videoList')
 
 const updatedVideos = []
-const allPromises = []
 const youtubeSearchUrl = "https://www.youtube.com/results?sp=EgIYAg%253D%253D&search_query="
 
-videoList.forEach(video => {
-  const redisKey = `youtube:${video}`
-  let latestVideo = {}
+Promise.all(
+  videoList.map(video => {
+    return new Promise(resolve => {
+      const redisKey = `youtube:${video}`
+      let latestVideo = {}
+      youtube.searchVideo({
+        part: 'id,snippet',
+        q: video,
+        maxResults: 10,
+        order: 'date',
+        type: 'video',
+        videoDuration: 'long'
+      }).then(results => {
+        if(results) {
+          items = results.items
+
+          if (!items.length) {
+            console.log(`No items found for: ${video}`)
+            return resolve(`No items found for: ${video}`)
+          }
   
-  allPromises.push(
-    youtube.searchVideo({
-      part: 'id,snippet',
-      q: video,
-      maxResults: 10,
-      order: 'date',
-      type: 'video',
-      videoDuration: 'long'
-    }).then(results => {
-      if(results) {
-        items = results.items
-        
-        if (!items.length) {
-          console.log(`No items found for: ${video}`)
-          return Promise.resolve(`No items found for: ${video}`)
-        }
-
-        latestVideo = findLatestVersion(items)
-
-        store.lookUpHash(redisKey, "publishedAt")
-          .then(storedItem => {
-            if (!storedItem) {
-              console.log(`=== ITEM NOT STORED: ${video} ===`)
-              console.log(`=== INSERTING DATA: ${video} ===`)
-              store.insertHash(redisKey, "id", latestVideo.id)
-              store.insertHash(redisKey, "publishedAt", latestVideo.publishedAt)
-            } else {
-              console.log(`=== ITEM FOUND IN STORE: ${video} ===`)
-              if (moment(storedItem) < moment(latestVideo.publishedAt)) {
-                console.log(`=== NEWER VERSION FOUND: ${video} - INSERTING NEW DATA`)
-                updatedVideos.push(`${youtubeSearchUrl}${video}`)
+          latestVideo = findLatestVersion(items)
+  
+          store.lookUpHash(redisKey, "publishedAt")
+            .then(storedItem => {
+              if (!storedItem) {
+                console.log(`=== ITEM NOT STORED: ${video} ===`)
+                console.log(`=== INSERTING DATA: ${video} ===`)
+                store.insertHash(redisKey, "id", latestVideo.id)
                 store.insertHash(redisKey, "publishedAt", latestVideo.publishedAt)
               } else {
-                console.log(`=== NO UPDATES FOUND: ${video} ===`)
-                console.log(`=== LATEST VERSION: ${storedItem}`)
+                console.log(`=== ITEM FOUND IN STORE: ${video} ===`)
+                if (moment(storedItem) < moment(latestVideo.publishedAt)) {
+                  console.log(`=== NEWER VERSION FOUND: ${video} - INSERTING NEW DATA`)
+                  updatedVideos.push(`${youtubeSearchUrl}${video}`)
+                  store.insertHash(redisKey, "publishedAt", latestVideo.publishedAt)
+                } else {
+                  console.log(`=== NO UPDATES FOUND: ${video} ===`)
+                  console.log(`=== LATEST VERSION: ${storedItem}`)
+                }
               }
-            }
-          })
-          .catch(err => {
-            console.log(`=== ERROR: ${video} ===`)
-            console.log(err)
-          })
-      }
+              return resolve()
+            })
+            .catch(err => {
+              console.log(`=== ERROR: ${video} ===`)
+              console.log(err)
+            })
+        } else {
+          return resolve()
+        }
+      })
     })
-  )
-})
-
-Promise.all(allPromises)
-  .then(() => {
-    console.log("Search done")
-    console.log("Updated videos are:")
-    console.log(updatedVideos)
-    process.exit()
   })
+).then(() => {
+  console.log("SEARCH DONE")
+  console.log(updatedVideos)
+})
 
 function findLatestVersion(items) {
   let latestVideo = {}
